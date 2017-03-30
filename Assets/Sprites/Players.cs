9d53d3c5b8be2a004afc37c2f1377f4b42f86c34
Unknown <cyen121@gmail.com>
2017-03-30 09:36:48 -0400

    delete unnecessary files

---
+++ Assets/Sprites/Players.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Players : MonoBehaviour {
+	public int health = 100;
+
+	new Rigidbody2D rigidbody; 
+
+
+	// Use this for initialization
+	void Start () {
+		
+		rigidbody = GetComponent<Rigidbody2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxisRaw ("Horizontal");
+		rigidbody.velocity = new Vector2 (x, 0);
+	}
+}
